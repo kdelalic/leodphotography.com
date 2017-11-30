b13000d71aedbe649ebe6bd047b76c06157b8645
kdelalic <delalik@mcmaster.ca>
2017-11-29 22:42:55 -0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+var theToggle = document.getElementById('toggle');
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
+
+jQuery(document).ready(function($){
+    'use strict';
+
+    $.Scrollax();
+});
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+    });
+    setTimeout(function() {   //calls click event after a certain time
+     $('#modalSubscription').modal('toggle')
+ }, 5000);
+    $('#close-modal').click(function(){
+        $('#modalSubscription').modal('toggle')
+    });
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
+    } // End if
+});
+});
+AOS.init();
+
+// click "Run" to replay
+
+var $body = $('body');
+
+var loading = [
+    { elements: $body, properties: { width: '20%' } },
+    { elements: $body, properties: { width: '30%' } },
+    { elements: $body, properties: { width: '50%' } },
+    { elements: $body, properties: { width: '100%' } },
+    { elements: $body, properties: { height: '100%' }, options: { 
+      complete: function () { 
+        $('.wrap').velocity( 'transition.slideUpIn' );
+        $('html').css({ background: '#fff' });
+      }
+    }
+  }
+]; 
+
+$.Velocity.RunSequence(loading);
