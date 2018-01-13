2e19d32307c0fcb229bb3e58cfc4d0e587b9dd63
delalik <delalik@mcmaster.ca>
2018-01-13 02:08:47 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
         $('#modalSubscription').modal('toggle')
     });
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("#scrolldownbutton").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
